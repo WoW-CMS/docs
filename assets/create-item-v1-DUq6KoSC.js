753a288d8a1f8b5e91920c38eb9694ed1bfc9239
DZywolf <34007722+DZywolf@users.noreply.github.com>
2024-03-26 17:36:05 +0000

    Deploying to gh-pages from @ WoW-CMS/docs@29127514a3be3bfe8b54aa0ac0feda47df5e4bbb 🚀

---
+++ assets/create-item-v1-DUq6KoSC.js
@@ -0,0 +1,1 @@
+const s="/docs/assets/images/create-category-v1.png",e="/docs/assets/images/create-item-v1.png";export{s as _,e as a};
---
+++ assets/create-realm-v1-DVrADxIA.js
@@ -0,0 +1,1 @@
+const s="/docs/assets/images/create-realm-v1.png";export{s as _};
